19857c7cb06fe3ef7c58083893cefc85a9b57ab0
ayanotakayuki <ayanotakayuki@gmail.com>
2016-12-07 08:58:59 +0900

    Update ボタン

--- gittest2/ViewController.swift
+++ gittest2/ViewController.swift
@@ -15,8 +15,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         //コードを記述１
-        
-        間違ったコード
     }
 
     override func didReceiveMemoryWarning() {
